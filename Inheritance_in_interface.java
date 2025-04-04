ad2935bde98b5283a6a7e885c769dd0578e8a736
Code-with-anvesha <sharmaanvesha905@gmail.com>
2025-04-04 21:52:57 +0530

    Added new files

---
+++ Inheritance_in_interface.java
@@ -0,0 +1,25 @@
+class Meme {
+    void laugh() {
+        System.out.println("Hahaha... Classic meme moment! ");
+    }
+}
+
+class Doge extends Meme {
+    void wow() {
+        System.out.println("Such inheritance, much wow! ");
+    }
+}
+
+class Catto extends Meme {
+    void angryMeow() {
+        System.out.println("Meow! Who touched my food?! ");
+    }
+}
+
+public class Inheritance_in_interface {
+    public static void main(String args[]) {
+        Catto c = new Catto();
+        c.angryMeow();
+        c.laugh();
+    }
+}
