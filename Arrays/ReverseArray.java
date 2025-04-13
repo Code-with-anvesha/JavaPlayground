3f1b17ef17c5a4215080c63b90f9ecb8af9958c9
Code-with-anvesha <sharmaanvesha905@gmail.com>
2025-04-13 21:18:32 +0530

    Added new file

---
+++ Arrays/ReverseArray.java
@@ -0,0 +1,25 @@
+public class ReverseArray {
+
+    public static String reverse(String name){
+
+     
+        String reversed = "";
+
+        for(int i = name.length()-1; i>=0; i--){
+
+            reversed += name.charAt(i);
+
+        }
+             return reversed;
+
+    }
+
+    public static void main(String[] args) {
+        
+        String name = "Anvesha Sharma";
+        String reversedName = reverse(name);
+        System.out.println("Reversed:" + reversedName);
+    }
+
+
+}
